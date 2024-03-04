d70906de27a0083b10bb3119c32598c483d1332a
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-04 14:20:03 +0530

    Hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,26 @@
+"use client";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 import React from "react";
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { id } = useParams();
+  const name = searchParams.get("name");
+  const pathName = usePathname();
+
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {pathName}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
