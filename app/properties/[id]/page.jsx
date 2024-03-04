f7c39975a225e5b838861684471cb984efc3e143
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-04 15:37:51 +0530

    Working on navbar

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,26 +1,7 @@
-"use client";
-import {
-  useParams,
-  usePathname,
-  useRouter,
-  useSearchParams,
-} from "next/navigation";
 import React from "react";
 
 const PropertyPage = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const { id } = useParams();
-  const name = searchParams.get("name");
-  const pathName = usePathname();
-
-  return (
-    <div>
-      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
-        Go Home {pathName}
-      </button>
-    </div>
-  );
+  return <div>PropertyPage</div>;
 };
 
 export default PropertyPage;
