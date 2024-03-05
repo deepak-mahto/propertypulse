3a0c1497d16e8e5e25d08a26ed90a96c1817b4d9
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-06 00:16:17 +0530

    Fetching data from server component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
 
-const PropertyPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
   return (
     <section class="px-4 py-6">
       <div class="container-xl lg:container m-auto px-4 py-6">
@@ -20,4 +35,4 @@ const PropertyPage = () => {
   );
 };
 
-export default PropertyPage;
+export default PropertiesPage;
