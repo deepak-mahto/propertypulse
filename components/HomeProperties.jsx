aa9cffd95c82308c79dbcbe49a657322b680bfa0
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-06 10:15:27 +0530

    Fetch single property data

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import PropertyCard from "./PropertyCard";
-
-async function fetchProperties() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
-    if (!res.ok) {
-      throw new Error("Failed to fetch the data");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+import { fetchProperties } from "@/utils/requests";
 
 const HomeProperties = async () => {
   const properties = await fetchProperties();
