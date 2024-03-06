e27f0ff2e0e9d6ab5e01d6ce14c2bf35a941970d
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-06 07:34:37 +0530

    Fetching data from server component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -19,19 +19,21 @@ const PropertiesPage = async () => {
   const properties = await fetchProperties();
 
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No Properties Found</p>
-        ) : (
-          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section class="px-4 py-6">
+        <div class="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No Properties Found</p>
+          ) : (
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
