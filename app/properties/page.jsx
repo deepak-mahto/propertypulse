6191949e911268808701d1ebae5f878d682e930e
Deepak Kumar <mahtodeepak857@gmail.com>
2024-03-09 06:38:20 +0530

    Display search results

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 import { fetchProperties } from "@/utils/requests";
 
 const PropertiesPage = async () => {
@@ -10,6 +11,12 @@ const PropertiesPage = async () => {
 
   return (
     <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+
       <section className="px-4 py-6">
         <div className="container-xl lg:container m-auto px-4 py-6">
           {properties.length === 0 ? (
--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -2,6 +2,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
@@ -35,8 +40,40 @@ const SearchResultsPage = () => {
     fetchSearchResults();
   }, [location, propertyType]);
 
-  console.log(properties);
-  return <div>SearchResultsPage</div>;
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            <Link
+              href="/properties"
+              className="flex items-center text-blue-500 hover:underline mb-3"
+            >
+              <FaArrowAltCircleLeft className="mr-2 mb-1" />
+              Back To Properties
+            </Link>
+            <h1 className="text-2xl mb-4">Search Results</h1>
+            {properties.length === 0 ? (
+              <p>No Search Results found</p>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            )}
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default SearchResultsPage;
